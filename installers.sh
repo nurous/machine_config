3e3e8af9900ea08b2b046ee9fd97fd351b5b5c78
David Reed <dreed@thoughtworks.com>
2016-09-13 16:20:01 +1000

    Removing ntfs-3g

--- installers.sh
+++ installers.sh
@@ -23,18 +23,4 @@ if [ ! `which zsh` ]; then
   brew install zsh
 fi
 
-if [ ! `which ntfs-3g` ]; then
-  echo "Installing NTFS-3G"
-  brew install ntfs-3g
-
-  # brew info fuse4x-kext
-  sudo cp -rfX /usr/local/Cellar/fuse4x-kext/0.9.1/Library/Extensions/fuse4x.kext /Library/Extensions
-  sudo chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x
-
-  # brew info ntfs-3g
-  sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.orig
-  sudo ln -s /usr/local/Cellar/ntfs-3g/2012.1.15/sbin/mount_ntfs /sbin/mount_ntfs
-fi
-
-
 echo "Additional programs to download the latest version of are listed at ~/Downloads/Installers/ManualDownloads.html"
